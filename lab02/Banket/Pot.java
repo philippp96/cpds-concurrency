820268a15ac3b92deab6f602050c5102ccede603
Philipp Panzer <philipp.panzer@fau.de>
2022-11-13 15:56:44 +0100

    add lab02 solution of warmup exercise

---
+++ lab02/Banket/Pot.java
@@ -0,0 +1,47 @@
+public class Pot {
+
+	private int servings = 0;
+	private int capacity;
+
+	public Pot(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public synchronized void getserving() throws InterruptedException {
+		// Condition synchronization: at least one serving available,
+		// otherwise, go to the Waiting Set til the cook fill the pot
+		while (servings == 0) {
+			System.out.println(Thread.currentThread().getName() + " has to wait");
+			wait();
+		}
+		--servings;
+
+		System.out.println(Thread.currentThread().getName() + " is served");
+
+		// when necessary, wake up threads in Waiting Set in order to asure
+		// a runnable cook
+		if (servings == 0)
+			notifyAll();
+
+		print_servings();
+	}
+
+	public synchronized void fillpot() throws InterruptedException {
+		//Condition synchronization: no servings in the pot
+		// otherwise, go to the Waiting Set til the pot is empty
+		while (servings > 0) {
+			System.out.println(Thread.currentThread().getName() + " has to wait");
+			wait();
+		}
+		servings = capacity;
+		System.out.println("cook fills the pot");
+		// wake up threads in Waiting Set in order to asure that the next savage will run
+		print_servings();
+		notifyAll();
+	}
+
+	//only for trace purposes
+	public synchronized void print_servings() {
+		System.out.println("servings in the pot: " + servings);
+	}
+}
