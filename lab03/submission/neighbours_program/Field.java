faa2c8504719133e7c2d6376639cd2b54885ea66
Philipp Panzer <philipp.panzer@fau.de>
2022-11-28 09:39:53 +0100

    add solution to neighbor code

--- lab03/submission/neighbours_program/Field.java
+++ lab03/submission/neighbours_program/Field.java
@@ -3,6 +3,9 @@ public class Field {
 		Flags flags = new Flags();
 		Thread a = new Neighbor(flags);
 		a.setName("alice");
-		...
+		Thread b = new Neighbor(flags);
+		b.setName("bob");
+		a.start();
+		b.start();
 	}
 }
--- lab03/submission/neighbours_program/Neighbor.java
+++ lab03/submission/neighbours_program/Neighbor.java
@@ -6,17 +6,17 @@ public class Neighbor extends Thread {
 	public void run() {
 		while (true) {
 			try {
-				String name = ... ;
+				String name = Thread.currentThread().getName();
 				System.out.println("try again, my name is: "+ name);
 				Thread.sleep((int)(200*Math.random()));
-				flags. ...;
-				if ( ... ) {
+				flags.set_true(name);
+				if (!flags.query_flag(name)) {
 					System.out.println(name + " enter");
 					Thread.sleep(400);
 					System.out.println(name + " exits");
 				}
 				Thread.sleep((int)(200*Math.random()));
-				flags ... ;
+				flags.set_false(name);
 			}
 			catch (InterruptedException e) {};
 		}
@@ -42,6 +42,4 @@ public class Neighbor extends Thread {
 		}
 	}
 	 */
-}
-
 }
